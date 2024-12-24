98d7dd13e316fd2045c5a68243df3095868d7e76
Sean Wang <seanwaterloo2997@gmail.com>
2024-12-24 16:14:22 -0500

    77: Extending the Parser

--- test.go
+++ test.go
@@ -1,7 +1,27 @@
 package main
 
+type Car struct {
+	name string
+	mileage int
+}
+
+func (c Car) printName() {
+	print(c.name)
+}
+
+func (c Car) honk() {
+	print("pdiuaerui")
+}
+
+
+
 func main() {
 	s := "a对"
 	// runeArray := []rune(s)[5]
 	print(len(s))
+
+
+	familyCar *int
+	somedudesCar Car
+	familyCar.printName()
 }
